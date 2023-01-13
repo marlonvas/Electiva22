abd46dcb82d3bbf255f3f8fab6fd2e6acc57b80a
marlonvas <mdvasquezss@gmail.com>
2023-01-12 19:57:18 -0500

    creacion del proyecto

---
+++ citas-react/src/App.jsx
@@ -0,0 +1,15 @@
+/*funcion creadora de la aplicacion */
+function App() {
+  
+ /*estado de retorno constsante */
+
+ 
+  return (
+    /*etiqueta padre y el body de la pagina */
+    <div className="App">
+      
+    </div>
+  )
+}
+/*export para importar el modulo a otras partes de la app */
+export default App

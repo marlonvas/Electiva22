f21cd7f390fb5dd94ee406c8137e73e49de9cd09
marlonvas <mdvasquezss@gmail.com>
2023-01-16 19:22:19 -0500

    cracion de formulario

--- citas-react/src/components/Formulario.jsx
+++ citas-react/src/components/Formulario.jsx
@@ -4,12 +4,23 @@ function Formulario() {
   return (
     <div className='md:w-1/2 lg:w-2/5'>
       <h2 className='font-black text-3xl text-center'> Seguimiento a pacientes</h2>
-      <p className=' text-lg mt-5 text-center'>
+      <p className=' text-lg mt-5 text-center mb-10'>
         Añade Pacientes{" "}
-      </p>
-      <span className='text-indigo-600 font-bold'>
+      
+      <span className='text-indigo-600 font-bold '>
         Administralos
       </span>
+      </p>
+
+      <form className='bg-white shadow-md rounded-lg py-10 px-5 mb-10'>
+        <div className='mb-5'>
+          <label htmlFor='mascota' className='block text-gray-700 uppercase font-bold'>Nombre Mascota</label>
+          <input  id='mascota' type="text" className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md' placeholder='Nombre de la Mascota'/>
+
+        </div>
+
+      </form>
+      
     </div>
     
   )

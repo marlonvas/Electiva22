ac4b09e6edabd184a9fdeb6525c4e410e7c288f9
marlonvas <mdvasquezss@gmail.com>
2023-01-17 19:34:26 -0500

    cambios formulario

---
+++ citas-react/src/components/Pacientes.jsx
@@ -0,0 +1,21 @@
+
+const Pacientes = () => {
+  return (
+    <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+        <p className="font-bold mb-3 text-gray-700 uppercase">Nombre:{" "} <span className='font-normal normal-case'>Rocko</span></p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Propietario:{" "} <span className='font-normal normal-case'>Marlon</span></p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Email:{" "} <span className='font-normal normal-case'>Md-vasquezs@corhuila.edu.co</span></p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Fecha de ingreso:{" "} <span className='font-normal normal-case'>17 de enero de 2023</span></p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Sintomas:{" "} <span className='font-normal normal-case'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque ex, exercitationem rerum et sit nemo explicabo labore delectus suscipit reiciendis? Fugit debitis corporis placeat rem consequatur sequi omnis architecto commodi a non magni, accusantium dolorem iste assumenda accusamus eum officia dicta et quasi quos aliquid odio exercitationem nisi repellendus. Ducimus?</span></p>
+
+
+
+
+
+
+
+      </div>
+  )
+}
+
+export default Pacientes
